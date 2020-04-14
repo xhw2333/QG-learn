a3622e7e446437e5d2776443bf7a3e826597c2f6
xhw2333 <1311844250@qq.com>
2020-04-14 13:37:44 +0800

    ajax and forms

--- smallhomework/first/js/login.js
+++ smallhomework/first/js/login.js
@@ -1,39 +1,39 @@
 function mySubmit() {
     var x = document.forms["Form"]["username"].value;
     var y = document.forms["Form"]["password"].value;
-    if((x == null || x == "") || (y == null || y == "")) {
-        // alert("无效！！");
-        if(x == null || x == "") {
-            alert("请输入用户名！！");
-        }
-        if(y == null || y == "") {
-            alert("请输入密码！！");
-        }
+    if(x == null || x == "") {
+        alert("请输入用户名！！");
         return false;
+    } else if(y == null || y == "") {
+        alert("请输入密码！！");
+        return false;
+    } else {
+        alert("SUCCESS");
     }
-    alert("正在登录.....");
 }
 
 function myRegister() {
     var a = document.forms["Form"]["username"].value;
     var b = document.forms["Form"]["password"].value; 
-    var c = document.forms["Form"]["password2"].value;
-    if((a == null || a == "") || (b == null || b == "") || (c == null || c == "")) {
-        // alert("无效！！");
-        if(a == null || a == "") {
-            alert("请输入用户名！！");
-        }
-        if(b == null || b == "" || c == null || c == "") {
-            alert("请输入密码！！");
-        }
-        // if(b != c){
-        //     alert("请确保密码一致");
-        // }
+    var c = document.forms["Form"]["phone"].value;
+    var d = document.forms["Form"]["email"].value;
+    var e = document.forms["Form"]["name"].value;
+    if(a == null || a == "") {
+        alert("请输入用户名！！");
         return false;
-    }
-    if(b != c){
-        alert("请确保密码一致");
+    } else if(b == null || b == "") {
+        alert("请输入密码！！");
         return false;
+    } else if(c == null || c == "") {
+        alert("请输入电话号码！！");
+        return false;
+    } else if(d == null || d == "") {
+        alert("请输入邮箱！！");
+        return false;
+    } else if(e == null || e == "") {
+        alert("请输入名字！！");
+        return false;
+    } else {
+        alert("注册成功");
     }
-    alert("注册成功");
 }
